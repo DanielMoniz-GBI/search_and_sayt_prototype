35b95ecb06e56265c4ef9f4f597750cb4b0a020e
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-05-09 12:20:49 -0400

    Fire SAYT event and catch it. Do not act on it yet.

--- component.js
+++ component.js
@@ -1,114 +1,55 @@
-
-function defineComponents() {
-  const paginationTemplate = document.createElement('template')
-  paginationTemplate.innerHTML = `
-    <style>
-      .selected {
-        color: red;
-      }
-    </style>
-    <span class="links">
-      <span class="left-buttons"><< <</span> <span class="page-numbers"><span class="selected">1</span> 2 3 4 5</span> <span class="right-buttons">> >></span>
-    </span>
-  `
-
-  const pagination = customElements.define('gbi-pagination', class Pagination extends HTMLElement {
+(function() {
+  class GBSearch extends HTMLElement {
     constructor() {
       super()
-      console.log('In constructor');
-    }
+      this.searchBox = undefined
 
-    static get observedAttributes() {
-      return ['page-num', 'num-items', 'num-pages', 'use-callback']
+      this.addEventListener('keyup', () => {
+        console.log('|' + this.searchBox.value + '|');
+        if (this.searchBox.value.length > 2) {
+          console.log('Triggering SAYT!');
+          this.fireEvent('gb-trigger-sayt', { 'searchTerm': this.searchBox.value })
+        }
+      })
     }
 
     connectedCallback() {
-      const pageNum = this.currentPageNumber
+      let placeholder = this.getAttribute('placeholder')
+      if (placeholder === null) {
+        placeholder = 'Search here'
+      }
+      this.innerHTML = `
+        <input class="gb-search-box" type='text' placeholder='${placeholder}'></input>
+      `
+      this.searchBox = this.querySelector('.gb-search-box')
+    }
+
+    fireEvent(name, data) {
+      const event = new CustomEvent(name, {
+        bubbles: true,
+        detail: data,
+      })
+      this.dispatchEvent(event)
+    }
+  }
+  customElements.define('gb-search', GBSearch)
+
+  class GBSayt extends HTMLElement {
+    constructor() {
+      super()
+      console.log('in constructor for GBSayt');
+      document.addEventListener('gb-trigger-sayt', (event) => {
+        console.log('Successfully caught SAYT trigger!');
+        console.log('Data:', event.detail);
+      })
+    }
+
+    connectedCallback() {
       console.log('In connectedCallback');
-      const pageNumbers = this.getPageNumbers(pageNum, this.numPages)
-      // this.appendChild(paginationTemplate.content.cloneNode())
-      this.innerHTML = `<span class="links"><< < ${pageNumbers} > >></span>`
+      this.innerHTML = `
+        <div>SAYT content goes here</div>
+      `
     }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-      console.time('pagination:attr-changed')
-      console.log('in attributeChangedCallback:', name, oldValue, newValue);
-      const pageNumbers = this.getPageNumbers(this.currentPageNumber, this.numPages)
-      this.innerHTML = `<span class="links"><< < ${pageNumbers} > >></span>`
-      // this.appendChild(paginationTemplate.content.cloneNode())
-      // const content = paginationTemplate.content.cloneNode()
-      // const numbers = content.querySelector('.page-numbers')
-      // numbers.in
-      console.timeEnd('pagination:attr-changed')
-    }
-
-    getPageNumbers(pageNum, numPages) {
-      const nums = []
-      let startNumber = Math.max(1, pageNum - 2)
-      let endNumber = Math.min(pageNum + 2, numPages)
-      let attributes = ""
-      console.log(this.getAttribute('click-number-callback'));
-      let useCallback = this.getAttribute('use-callback')
-      window.gbiHandlePaginationClick = (event) => {
-        console.log('in gbiHandlePaginationClick');
-        event.preventDefault()
-        const clickedEvent = new CustomEvent('gbi:pagination-clicked', { detail: { selection: -5 } })
-        document.dispatchEvent(clickedEvent)
-      }
-      console.log('useCallback:', useCallback);
-      if (useCallback) {
-        attributes += ` onclick="window.gbiHandlePaginationClick(event)"`
-      }
-      // this.getAttribute('click-number-callback')}
-      for (let i = startNumber; i <= endNumber; i++) {
-        if (i === pageNum) {
-          nums.push(`<span class='selected'>${i}</span>`)
-          continue
-        }
-        const url = new URL(window.location)
-        url.searchParams.set(this.pageKey, i)
-        nums.push(`<a href="${url.href}"${attributes}>${i}</a>`)
-      }
-      return nums.join(" ")
-    }
-
-    get currentPageNumber() {
-      let pageNum = this.getAttribute('page-num')
-      if (pageNum) return parseInt(pageNum)
-      const url = new URL(window.location)
-      if (url.searchParams.has('page')) {
-        return parseInt(url.searchParams.get('page'))
-      } else if(url.searchParams.has('pg')) {
-        return parseInt(url.searchParams.get('pg'));
-      }
-      return 1;
-    }
-
-    get numPages() {
-      return parseInt(this.getAttribute('num-pages'))
-        || Math.ceil(this.numItems / this.pageSize)
-    }
-
-    get numItems() {
-      return parseInt(this.getAttribute('num-items')) || 1
-    }
-
-    get pageSize() {
-      return parseInt(this.getAttribute('page-size')) || 12
-    }
-
-    get pageKey() {
-      return this.getAttribute('page-key') || 'page'
-    }
-
-    set clickCallback(callback) {
-      console.log('Click callback being set!');
-      this.clickNumberCallback = (event) => {
-        event.preventDefault()
-        clickCallback(-7)
-      }
-    }
-  })
-}
-
-defineComponents()
+  }
+  customElements.define('gb-sayt', GBSayt)
+})()
