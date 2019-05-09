e816e2a555db300c7d3eb259cb4638b413f97010
Daniel Moniz <daniel.moniz@groupbyinc.com>
2019-05-09 17:12:39 -0400

    WIP: Add simple cache to logic layer.

--- groupby/logicLayer.js
+++ groupby/logicLayer.js
@@ -1,3 +1,15 @@
+const cache = (function() {
+  const cache = {}
+  return {
+    get: function(key) {
+      return cache[key]
+    },
+    set: function(key, value) {
+      cache[key] = value
+    },
+  }
+})();
+
 (function() {
   console.log('Logic layer loaded...');
   document.addEventListener('gb-request-sayt-suggestions', (event) => {
@@ -8,22 +20,35 @@
     const searchTerm = event.detail.searchTerm
     if (!searchTerm) { return }
 
+    const cachedResponse = cache.get(searchTerm)
+    if (cachedResponse) {
+      const responseEvent = new CustomEvent('gb-provide-sayt-suggestions', {
+        bubbles: true,
+        detail: cachedResponse,
+      })
+      return dispatchEvent(responseEvent)
+    }
+
     const quantity = event.detail.quantity || 5
     const products = window.mockData.filter(product => {
       return product.name.toLowerCase().startsWith(searchTerm.toLowerCase())
     }).slice(0, quantity)
 
+    const searchInfo = {
+      searchTerm,
+      quantity,
+      products,
+    }
     const responseEvent = new CustomEvent('gb-provide-sayt-suggestions', {
       bubbles: true,
-      detail: {
-        searchTerm,
-        quantity,
-        products,
-      }
+      detail: searchInfo,
     })
     console.log('responseEvent:', responseEvent);
     setTimeout(() => {
       dispatchEvent(responseEvent)
+      cache.set(searchTerm, searchInfo)
     }, 400)
   })
+
+
 })()
